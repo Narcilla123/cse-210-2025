e770bce24ae0f3eb5772e44c1831ae81f90cd2b4
Nn1fty <arc24004@byui.edu>
2025-10-30 09:13:07 -0600

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Please enter your First Name");
+        firstName = Console.ReadLine();
+        Console.Write("Please enter your Last Name");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Please enter your First Name");
+        firstName = Console.ReadLine();
+        Console.Write("Please enter your Last Name");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
